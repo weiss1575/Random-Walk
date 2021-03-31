97357cf8084f3a02f5beff83c049717f8f25cc6c
Matt Weiss <weiss1575@gmail.com>
2021-03-30 20:46:51 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Random Walk
+A python program that generates random walks and creates a visualization of the data using Matplotlib.  
+
+## About
+A random walk is a path that has no clear direction. Every random walk is different.  
+- The green dot represents the starting position for the walk.   
+- The red dot represents the ending position for the walk.  
+- A color map is used to show the order of the points in the walk. The walk starts at light blue and the points get darker as the walk progresses.
+
+## How to Run
+Simply clone this repo and type python rw_visual.py
---
+++ rw_visual.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as p
+from rw import RandomWalk
+
+
+while True:
+    rw = RandomWalk(5_000)
+    rw.fill_walk()
+
+    p.style.use('classic')
+    # Numbers for color map
+    point_numbers = range(rw.num_points)
+    # Plot random walk data
+    fig, ax = p.subplots(figsize=(15,9), dpi=96)
+
+    im = ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=p.cm.Blues,
+        edgecolors='none', s = 10)
+
+    # Plot beginning and end points
+    ax.scatter(0, 0, c='green', edgecolors='none', s = 50)
+    ax.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=50)
+    # Remove the axes
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+    p.colorbar(im, ax=ax)
+    p.show()
+
+    keep_going = input("Generate another walk? (y/n): ").lower()
+    if (keep_going == 'n'):
+        break
